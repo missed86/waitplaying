255a6099f2bb50cdfd29629623f1ba1e7d32c0f7
missed86 <jimmy11286@gmail.com>
2023-01-19 21:58:28 +0100

    cambios

--- front-end/src/components/Home/GameList.jsx
+++ front-end/src/components/Home/GameList.jsx
@@ -4,17 +4,16 @@ import GameCard from "../GameCard";
 import "./GameList.css";
 
 export default function GameList({ games, date }) {
-  return (
-    <div className="GameGroup">
-    <span>{date}</span>
-    <div className="GameList">
-      {games.map(({ title, cover, slug, platforms, id }) => (
-        <Link key={id} to={`/game/${slug}`} className="no-link">
-          <GameCard image={cover} title={title} platforms={platforms} />
-        </Link>
-      ))}
-    </div>
-
-    </div>
-  );
+	return (
+		<div className="GameGroup">
+			<div className="date">{date}</div>
+			<div className="GameList">
+				{games.map(({ title, cover, slug, platforms, id }) => (
+					<Link key={id} to={`/game/${slug}`} className="no-link">
+						<GameCard image={cover} title={title} platforms={platforms} />
+					</Link>
+				))}
+			</div>
+		</div>
+	);
 }
