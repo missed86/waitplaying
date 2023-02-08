a55e20a1d4db89e568fcabbbe6cb14a967ed510d
missed86 <jimmy11286@gmail.com>
2023-02-08 07:27:37 +0100

    refactorizando scrapper de gp

--- back-end/api/GPScrapper.py
+++ back-end/api/GPScrapper.py
@@ -4,7 +4,6 @@ import time
 import re
 import json
 
-mainURL = r"https://displaycatalog.mp.microsoft.com/v7.0/products?bigIds={ids}&market=US&languages=en-us&MS-CV=DGU1mcuYo0WMMp"
 console_URL = 'https://catalog.gamepass.com/sigls/v2?id=f6f1f99f-9b49-4ccd-b3bf-4d9767a77f5e&language=en-us&market=US'
 
 def slugger(string):
@@ -19,29 +18,33 @@ def GamePassScrapper(URL):
     https://catalog.gamepass.com/sigls/v2?id=f6f1f99f-9b49-4ccd-b3bf-4d9767a77f5e&language=en-us&market=US
 
     """
-
-
-    response = requests.get(URL)
+    GamepassAllGamesJSON = requests.get(URL)
     output = response.json()
 
-    return output
+    GamepassAllGamesList = []
+    for game in GamepassAllGamesJSON.json():
+        # GamepassAllGamesList.append(game['id'])
+        if 'id' in game:
+            GamepassAllGamesList.append(game['id'])
+    idsString = ",".join(GamepassAllGamesList)
+
+    return idString
 
 def requestData(ids):
     object = dict()
-    for id in ids[1:]:
-        id= id['id']
-        URL = f'https://displaycatalog.mp.microsoft.com/v7.0/products?bigIds={id}&market=ES&languages=en-us&MS-CV=DGU1mcuYo0WMMp'
-        response = requests.get(URL).json()
-        responseID = response['Products'][0]['ProductId']
+    URL = f'https://displaycatalog.mp.microsoft.com/v7.0/products?bigIds={ids}&market=ES&languages=en-us&MS-CV=DGU1mcuYo0WMMp'
+    response = requests.get(URL).json()
+    for game in response['Products']:
+        responseID = game[0]['ProductId']
         object[responseID] = {
-            'title': response['Products'][0]['LocalizedProperties'][0]['ProductTitle'],
-            'short_title': response['Products'][0]['LocalizedProperties'][0]['ShortTitle'],
-            'end_date': response['Products'][0]['DisplaySkuAvailabilities'][0]['Availabilities'][0]['Conditions']['EndDate'],
-            'start_date': response['Products'][0]['DisplaySkuAvailabilities'][0]['Availabilities'][0]['Conditions']['StartDate'],
-            'slug': slugger(response['Products'][0]['LocalizedProperties'][0]['ProductTitle']),
+            'title': game[0]['LocalizedProperties'][0]['ProductTitle'],
+            'short_title': game[0]['LocalizedProperties'][0]['ShortTitle'],
+            'end_date': game[0]['DisplaySkuAvailabilities'][0]['Availabilities'][0]['Conditions']['EndDate'],
+            'start_date': game[0]['DisplaySkuAvailabilities'][0]['Availabilities'][0]['Conditions']['StartDate'],
+            'slug': slugger(game[0]['LocalizedProperties'][0]['ProductTitle']),
             'console': True
         }
-        print(slugger(object[responseID]['title']), object[responseID]['title'])
+    print(slugger(object[responseID]['title']), object[responseID]['title'])
     
     return object
         
